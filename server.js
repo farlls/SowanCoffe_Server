52daa09eaf6713d170066dba55b3b15937e392fe
farlls <farrelshahizdn22@gmail.com>
2023-07-14 01:03:09 +0700

    Rename env

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
 });
 // Endpoints untuk API
 app.get("/menu/:value", (req, res) => {
