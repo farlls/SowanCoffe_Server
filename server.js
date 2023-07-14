1290bd786432c1b1de2036cf398ef19aaa260b19
farlls <farrelshahizdn22@gmail.com>
2023-07-14 18:21:09 +0700

    Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3301;
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 
